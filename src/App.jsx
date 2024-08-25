717cbe27a85255e178833a67e166b8b1b699413a
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-08-26 03:22:46 +0500

    Todo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,86 @@
-import { useState , useRef } from 'react'
-import './App.css'
-
+import { useState, useRef } from "react";
+import "./App.css";
 
 // Create Component
 function App() {
   const [todo, setTodo] = useState([]);
   const todoVal = useRef();
 
-  // To add  todo 
-  const addTodo = (event) =>{
-    if (todo !==  " ") {
-      event.preventDefault();
-      todo.push(todoVal.current.value);
-      setTodo([...todo])
-      console.log(todo);
-      todoVal.current.value = "";
-    }
+  // To add  todo
+  const addTodo = (event) => {
+    event.preventDefault();
 
-    else{
+    if (todoVal.current.value === " ") {
       alert("Please Enter todo");
     }
-  }
+   
+     else {
+      todo.push(todoVal.current.value);
+      setTodo([...todo]);
+      console.log(todo);
+      todoVal.current.value = "";
+    
+    }
+  };
 
   // To Edit Todo
-  const EditTodo = (index) =>{
+  const EditTodo = (index) => {
     const editVal = prompt("Enter Updated Value");
-    todo.splice(index , 1 ,editVal)
-    setTodo([...todo])
-  }
+    todo.splice(index, 1, editVal);
+    setTodo([...todo]);
+  };
 
   // To Delete Todo
-  const DeletTodo = (index) =>{
-      todo.splice(index , 1);
-      setTodo([...todo])
-  }
-  
+  const DeletTodo = (index) => {
+    todo.splice(index, 1);
+    setTodo([...todo]);
+  };
 
   return (
     <>
-    <div className='container'>
-    <h2>Todo</h2>
+      <div className="container">
+        <h2>Todo</h2>
 
-    {/* Todo form  */}
-    <form className='styled-form' onSubmit={addTodo}>
-  <div className="form-group">
+        {/* Todo form  */}
+        <form className="styled-form" onSubmit={addTodo}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control width-25px"
+              placeholder="Enter Todo"
+              ref={todoVal}
+            />
+          </div>
 
-    <label className="form-label">Enter Todo</label>
-    <input type="text" className="form-control width-25px" placeholder='Todo' ref={todoVal}/>
+          <button className="btn-add" type="submit"> Add Todo </button>
+        </form>
 
-    </div>
+        {/* Todo List  */}
 
-    <button type='submit'> Add Todo </button>
-
-  </form>
-
-    {/* Todo List  */}
-
-  <ul className='styled-list'>
-   { todo.map((item , index) =>{
-      return <div key={index}>
-      <li>
-        {item}
-      </li>
-  <button onClick={()=> EditTodo(index)} type="submit" className="btn-success">Edit</button>
-  <button onClick={() => DeletTodo(index)} type="submit" className="btn-danger">Delete</button>
-  </div>
-})}
-  </ul>
-
-</div>
+        <ul className="styled-list">
+          {todo.map((item, index) => {
+            return (
+              <div key={index}>
+                <li>{item}</li>
+                <button
+                  onClick={() => EditTodo(index)}
+                  type="submit"
+                  className="btn-success">
+                  Edit
+                </button>
+                <button
+                  onClick={() => DeletTodo(index)}
+                  type="submit"
+                  className="btn-danger">
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
     </>
-)
+  );
 }
 
-
-export default App
+export default App;
